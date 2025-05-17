a80b570d334433a8552b2b795005ab4a7528f76a
jesusalc <jesusalc@gmail.com>
2025-05-17 20:37:53 +0200

    added shell runners with queue and kill

--- dev.sh
+++ dev.sh
@@ -1,3 +1,32 @@
-tail -f storage/logs/fs-watcher.log &
+#!/usr/bin/env bash
+
+
+set -xuEe
+
+function on_exit(){
+  kill $(ps aux | grep queue:work | grep -v grep  | awk '{print $2}' | xargs)
+  kill $(ps aux | grep  fs:watch| grep -v grep | awk '{print $2}' | xargs)
+
+} # end on_exit
+
+trap 'on_exit' ERR INT EXIT
+
+
+
+typeset -i _pidt=0
+typeset -i _pidw=0
+
+tail -f storare/logs/fs-watcher.log &
+_pidt=$!
+php artisan queue:work &
+_pidw=$!
+
 php artisan fs:watch
 pkill tail
+kill ${_pidt}
+kill ${_pidw}
+kill $(ps aux | grep queue:work | grep -v grep | awk '{print $2}' | xargs)
+kill $(ps aux | grep  fs:watch | grep -v grep | awk '{print $2}' | xargs)
+
+
+#
--- tests.bash
+++ tests.bash
@@ -1,5 +1,19 @@
+#!/usr/bin/env bash
+
 set -uEex
 clear
+
+php artisan queue:work &
+
+
+function on_exit(){
+  kill $(ps aux | grep queue:work | grep -v grep | awk '{print $2}' | xargs)
+	kill $(ps aux | grep  fs:watch | grep -v grep  | awk '{print $2}' | xargs)
+
+} # end on_exit
+
+trap 'on_exit' ERR INT EXIT
+
 php artisan test --filter=TxtAppenderServiceTest
 php artisan test --filter=JsonWebhookServiceTest
 php artisan test --filter=MemeRestorerServiceTest
@@ -7,3 +21,7 @@ printf '\xFF\xD8\xFF\xDB' > tests/Unit/Services/FileWatchers/test.jpg
 php artisan test --filter=ImageOptimizerServiceTest
 rm -rf tests/Unit/Services/FileWatchers/test.jpg
 php artisan test --filter=ZipExtractorServiceTest
+
+
+
+#
