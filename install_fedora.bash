950045426ef08a57d6b47eb7bf83e399f9227e55
jesusalc <jesusalc@gmail.com>
2025-05-17 17:50:28 +0200

    updated fedora and ubuntu installs with notify extra pecl

--- install_fedora.bash
+++ install_fedora.bash
@@ -1,2 +1,8 @@
+set -xuEe
 sudo dnf install jpegoptim optipng pngquant gifsicle -y
 npm install -g svgo
+pecl install inotify
+php -i | grep php.ini
+echo "extension=inotify" >> "${HOME}/../linuxbrew/.linuxbrew/etc/php/8.4/php.ini"
+
+#
--- install_ubuntu.bash
+++ install_ubuntu.bash
@@ -1,1 +1,6 @@
+set -xuEe
 sudo apt-get install jpegoptim optipng pngquant gifsicle svgo -y
+npm install -g svgo
+pecl install inotify
+php -i | grep php.ini
+echo "extension=inotify" >> "${HOME}/../linuxbrew/.linuxbrew/etc/php/8.4/php.ini"
